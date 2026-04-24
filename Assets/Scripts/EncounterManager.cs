9f9cd7d321ddcf49a079f186b95fffebb3fbd943
chillbok <kylejunho@gmail.com>
2026-04-25 03:36:04 +0900

    feat: 인카운터 씬 기초적인 것들 완성
    이제 기초적인 것들은 완성했으니, 본격적인 전투를 구현해야 함

---
+++ Assets/Scripts/EncounterManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class EncounterManager : MonoBehaviour
+{
+    public static EncounterManager Instance { get; private set; }
+    
+    // 보낼 변수
+    public PlayerStatController PlayerStatController {get; set;}
+    public EnemyStatController EnemyStatController {get; set;}
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        
+        Instance = this;
+    }
+}
